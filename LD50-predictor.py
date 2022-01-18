148796ba25da18f1a173591888b202a6a310dc59
raphaelolipitz <raphaelolipitz@gmx>
2022-01-18 19:31:18 +0100

    Some preprocessing for testing

--- LD50-predictor.py
+++ LD50-predictor.py
@@ -2,16 +2,13 @@
 
 from rdkit import Chem
 from rdkit.Chem import AllChem
-from rdkit.Chem import PandasTools
+from rdkit.Chem import PandasTools #
 import pandas as pd
 from IPython.core.display import HTML
 import os
 from rdkit import RDConfig
 from sklearn.model_selection import train_test_split
 from numpy import loadtxt
-from keras.models import Sequential
-from keras.layers import Dense
-
 import torch.autograd as autograd         # computation graph
 from torch import Tensor                  # tensor node in the computation graph
 import torch.nn as nn                     # neural networks
@@ -21,30 +18,52 @@ from torch.jit import script, trace       # hybrid frontend decorator and tracin
 
 
 
-SDFFile = "/Users/raphael/Downloads/qspr-dataset-02.sdf"
+SDFFile = "/Users/raphael/Documents/Studium/Vorlesungen/Cheminfo/cheminformatics/qspr-dataset-02.sdf 2"
 Data = PandasTools.LoadSDF(SDFFile,smilesName='SMILES',
                            molColName='Molecule',
                            includeFingerprints=True,
                            removeHs=False, strictParsing=True)
 
-Data.info()
 
-smi = []
-mols = [mol for mol in Chem.SDMolSupplier(SDFFile)]
+def datapreprossessing():
+    #compute the SMILES repressentation for the molecules
+    smi = []
+    mols = [mol for mol in Chem.SDMolSupplier(SDFFile)]
+    for mol in mols:
+        smi.append(Chem.MolToSmiles(mol))
 
-for mol in mols:
-    smi.append(Chem.MolToSmiles(mol))
+    #calculate the MorgenFingerprint as Vector for the molocules? Maybe a nother Fingerprint would be better.
+    fp = []
+    for mol in mols:
+        fp.append(AllChem.GetMorganFingerprintAsBitVect(mol, useChirality=True, radius=2, nBits = 124))
 
-fp = []
-for mol in mols:
-    fp.append(AllChem.GetMorganFingerprintAsBitVect(mol, useChirality=True, radius=2, nBits = 124))
+    Data["SMILES"] =smi
+    Data["Fingerprints"] = fp
+
+    print(Data.info())
+    print(Data.head())
+
+    #one of the common ratios for splitting the data.
+    train_ratio = 0.70
+    validation_ratio = 0.15
+    test_ratio = 0.15
 
 
-def function():
-    return
+    x_train, x_test, y_train, y_test = train_test_split(Data, Data, test_size= 1 - train_ratio)
+    x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size= test_ratio / (test_ratio + validation_ratio))
+
+    print(x_train, x_val, x_test)
+
+    return Data
+
+
+
+#start of the funktions
+def main():
+    datapreprossessing()
+
+
 
 #start of the program
-def main():
-    function()
 if __name__ == "__main__":
     main()
