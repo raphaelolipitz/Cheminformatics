a9ec10013fcb2a4594ef6846edc3985b452313e9
raphaelolipitz <raphaelolipitz@gmx>
2022-01-19 10:12:24 +0100

    First definition of the Layers and the optimizer

--- LD50-predictor.py
+++ LD50-predictor.py
@@ -1,5 +1,5 @@
 #install all packages for the predictor
-
+import torch
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem import PandasTools
@@ -40,8 +40,7 @@ def datapreprossessing():
     Data["SMILES"] =smi
     Data["Fingerprints"] = fp
 
-    print(Data.info())
-    print(Data.head())
+
 
 
 
@@ -54,7 +53,7 @@ def datapreprossessing():
     x_train, x_test, y_train, y_test = train_test_split(Data, Data, test_size= 1 - train_ratio)
     x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size= test_ratio / (test_ratio + validation_ratio))
 
-    print(x_train, x_val, x_test)
+
 
     return Data
 
@@ -63,70 +62,48 @@ class Neural_Network(nn.Module):
 
     def __init__(self, ):
         super(Neural_Network, self).__init__()
+
         # parameters
-        # TODO: parameters can be parameterized instead of declaring them here
-        self.inputSize = 2
+        self.inputSize = 124
         self.outputSize = 1
-        self.hiddenSize = 3
+        self.hiddenSize = 124
 
-        # weights
-        self.W1 = torch.randn(self.inputSize, self.hiddenSize)  # 2 X 3 tensor
-        self.W2 = torch.randn(self.hiddenSize, self.outputSize)  # 3 X 1 tensor
+        # Defined Layer imputlayer is as big as the Fingerprint(124bits). Outputlayer is 1 for one LD50 value.
+        self.input_layer = nn.Linear(self.inputSize, self.hiddenSize)
+        self.hidden_layer = nn.Linear(self.hiddenSize, self.hiddenSize)
+        self.output_layer = nn.Linear(self.hiddenSize, self.outputSize)
 
-    def forward(self, X):
-        self.z = torch.matmul(X, self.W1)  # 3 X 3 ".dot" does not broadcast in PyTorch
-        self.z2 = self.sigmoid(self.z)  # activation function
-        self.z3 = torch.matmul(self.z2, self.W2)
-        o = self.sigmoid(self.z3)  # final activation function
-        return o
-
-    def sigmoid(self, s):
-        return 1 / (1 + torch.exp(-s))
-
-    def sigmoidPrime(self, s):
-        # derivative of sigmoid
-        return s * (1 - s)
-
-    def backward(self, X, y, o):
-        self.o_error = y - o  # error in output
-        self.o_delta = self.o_error * self.sigmoidPrime(o)  # derivative of sig to error
-        self.z2_error = torch.matmul(self.o_delta, torch.t(self.W2))
-        self.z2_delta = self.z2_error * self.sigmoidPrime(self.z2)
-        self.W1 += torch.matmul(torch.t(X), self.z2_delta)
-        self.W2 += torch.matmul(torch.t(self.z2), self.o_delta)
-
-    def train(self, X, y):
-        # forward + backward pass for training
-        o = self.forward(X)
-        self.backward(X, y, o)
-
-    def saveWeights(self, model):
-        # we will use the PyTorch internal storage functions
-        torch.save(model, "NN")
-        # you can reload model with all the weights and so forth with:
-        # torch.load("NN")
-
-    def predict(self):
-        print("Predicted data based on trained weights: ")
-        print("Input (scaled): \n" + str(xPredicted))
-        print("Output: \n" + str(self.forward(xPredicted)))
+    def forward(self, x):
+        #for the acivation ReLu seamse to be a good chois
+        x = F.relu(self.input_layer)
+        x = F.relu(self.hidden_layer)
+        x = self.output_layer
+        return x
 
 
 
 
 
 
-NN = Neural_Network()
-for i in range(1000):  # trains the NN 1,000 times
-    print ("#" + str(i) + " Loss: " + str(torch.mean((y - NN(X))**2).detach().item()))  # mean sum squared loss
-    NN.train(X, y)
-NN.saveWeights(NN)
-NN.predict()
+
+
 
 
 #start of the funktions
 def main():
     datapreprossessing()
+
+
+    Predictor = Neural_Network()
+    print(Predictor)
+    optimizer = optim.Adam(Predictor.parameters(), lr = 0.001)
+
+    epochs = 1000
+
+    for i in range(epochs):  # trains the NN 1,000 times
+        X , y = data
+        Predictor.zero_grad()
+
 
 
 
